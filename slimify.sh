ce60ed0b5aa98be76f015b2d3efdc5c8bf7ccd76
Weston Steimel <weston.steimel@gmail.com>
2021-03-02 23:10:54 +0000

    update for pydantic 1.8
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- slimify.sh
+++ slimify.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pip install -U pip==20.2.4
+
 PIP_DOWNLOAD_CMD="pip download --no-deps --disable-pip-version-check"
 
 mkdir -p dist
@@ -10,7 +12,8 @@ mkdir -p dist
     cd dist
 
     if [[ -z "${PYDANTIC_VERSION}" ]]; then
-        PYDANTIC_VERSION=$(pip search pydantic | pcregrep -o1 -e "^pydantic \((.*)\).*$")
+        echo "Set the PYDANTIC_VERSION environment variable."
+        exit 1
     fi
 
     echo "slimming wheels for pydantic version ${PYDANTIC_VERSION}"
@@ -18,7 +21,11 @@ mkdir -p dist
     $PIP_DOWNLOAD_CMD --python-version 3.9 --platform manylinux2014_x86_64 pydantic==${PYDANTIC_VERSION}
     $PIP_DOWNLOAD_CMD --python-version 3.8 --platform manylinux2014_x86_64 pydantic==${PYDANTIC_VERSION}
     $PIP_DOWNLOAD_CMD --python-version 3.7 --platform manylinux2014_x86_64 pydantic==${PYDANTIC_VERSION}
-    $PIP_DOWNLOAD_CMD --python-version 3.6 --platform manylinux2014_x86_64 pydantic==${PYDANTIC_VERSION}
+
+    # We can't specify `--python-version` for the 3.6 version because there is some strange bug which prevents
+    # it from finding matching packages, so instead we just use a 3.6 base image
+    $PIP_DOWNLOAD_CMD --platform manylinux2014_x86_64 pydantic==${PYDANTIC_VERSION}
+    #$PIP_DOWNLOAD_CMD --python-version 3.6 --platform manylinux2014_x86_64 pydantic==${PYDANTIC_VERSION}
 
     for filename in ./*.whl
     do
@@ -30,7 +37,12 @@ mkdir -p dist
     done
 
     pip uninstall -y --disable-pip-version-check pydantic
-    pip install --disable-pip-version-check pydantic==${PYDANTIC_VERSION} -f .
+    pip install \
+        --disable-pip-version-check \
+        pydantic==${PYDANTIC_VERSION} \
+        -f . \
+        --index-url https://westonsteimel.github.io/pypi-repo \
+        --extra-index-url https://pypi.org/pypi
 
     python -c "
 import pydantic
